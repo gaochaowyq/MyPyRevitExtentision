20a0963a06285dcf5bbd964fc71609e525dd2d1d
unknown <2016028@cadg.com>
2019-03-17 12:35:00 +0800

    change

--- MyExtenstion.extension/Gaochao.tab/Gaochao.panel/Annotation_Modify.pulldown/Grid_3DTo2D.pushbutton/script.py
+++ MyExtenstion.extension/Gaochao.tab/Gaochao.panel/Annotation_Modify.pulldown/Grid_3DTo2D.pushbutton/script.py
@@ -2,11 +2,11 @@
 __doc__="返回选择物体的类型"
 import rpw
 from rpw import revit, DB, UI,db,doc
-from System.Collections.Generic import List
-import json
-from scriptutils import this_script
-from scriptutils.userinput import CommandSwitchWindow
-import subprocess as sp
+#from System.Collections.Generic import List
+#import json
+#from scriptutils import this_script
+#from scriptutils.userinput import CommandSwitchWindow
+#import subprocess as sp
 #Change Selected Grid From 3D to 2D
 
 #selection = rpw.ui.Selection().elements
--- MyExtenstion.extension/Gaochao.tab/Gaochao.panel/Test.pulldown/ReturnTypeOfSelection.pushbutton/script.py
+++ MyExtenstion.extension/Gaochao.tab/Gaochao.panel/Test.pulldown/ReturnTypeOfSelection.pushbutton/script.py
@@ -11,9 +11,40 @@ from Helper import *
 import math
 from pyrevit import forms
 from pyrevit.framework import Stopwatch
+curview = revit.activeview
+curdoc=revit.doc
 
 
 
-selection = revit.pick_element()
+selection = revit.pick_elements()
+options=DB.Options()
+def GetElementSolid(Element):
+    Solids=[]
+    geo=Element.get_Geometry(options)
+    for i in geo:
 
-print(selection.UniqueId)
+        if type(i) != DB.GeometryInstance:
+            if i.Volume!=0:
+                Solids.append(i)
+    return Solids
+
+walls=[selection[0]]
+beams=[selection[1]]
+
+with db.Transaction('ChageWall'):
+    for w in walls:
+        for b in beams:
+            try:
+                wSolid=GetElementSolid(w)[0]
+                bSolid = GetElementSolid(b)[0]
+                result=DB.BooleanOperationsUtils.ExecuteBooleanOperation (wSolid,bSolid,DB.BooleanOperationsType.Intersect)
+                if result.Volume>0.0001:
+                    DB.JoinGeometryUtils.JoinGeometry(curdoc,w,b)
+                    print("Id{}与Id{}成功剪切".format(w.Id, b.Id))
+
+            except:
+                print("Id{}或Id{}有问题请查看".format(w.Id,b.Id))
+
+#print(GetElementSolid(selection))
+
+print("完成所有剪切")
